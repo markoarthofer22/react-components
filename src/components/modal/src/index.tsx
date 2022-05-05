4ae0d3457f728092a240dac4a461fd0bdeccad60
markoarthofer22 <markoarthofer95@gmail.com>
2022-05-05 12:48:29 +0200

    feat: updated modal to use framer-motion

--- src/components/modal/src/index.tsx
+++ src/components/modal/src/index.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { useTheme } from '@emotion/react';
 import { VscChromeClose } from 'react-icons/vsc';
+import { motion, AnimatePresence } from 'framer-motion';
 import { ModalStyles } from './styles';
 
 export interface IModalProps {
@@ -25,6 +26,27 @@ export const Modal: React.FC<IModalProps> = ({
 
     const CloseIcon = icon || VscChromeClose;
 
+    const dropIn = {
+        hidden: {
+            y: '-100vh',
+            opacity: 0,
+        },
+        active: {
+            y: '0',
+            opacity: 1,
+            transition: {
+                duration: 0.1,
+                type: 'spring',
+                damping: 25,
+                stiffness: 500,
+            },
+        },
+        exit: {
+            y: '-100vh',
+            opacity: 0,
+        },
+    };
+
     React.useEffect(() => {
         const closeOnEsc = (e?: any) => {
             e.stopPropagation();
@@ -42,25 +64,44 @@ export const Modal: React.FC<IModalProps> = ({
     }, []);
 
     return (
-        <div css={ModalStyles(theme)}>
-            <div
-                className={`${className}--wrapper ${
-                    visible ? `${className}--wrapper-active` : ''
-                }`}
-            >
-                <div className={`${className}--content`}>
-                    <div className={`${className}--header`}>
-                        {title && (
-                            <h2 className={`${className}--header--title`}>
-                                {title}
-                            </h2>
-                        )}
-                        <CloseIcon onClick={() => onModalClose()} />
-                    </div>
-                    <div className={`${className}--children`}>{children}</div>
+        <AnimatePresence initial={false} exitBeforeEnter>
+            {visible && (
+                <div css={ModalStyles(theme)}>
+                    <motion.div
+                        initial={{
+                            opacity: 0,
+                        }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className={`${className}--wrapper ${
+                            visible ? `${className}--wrapper-active` : ''
+                        }`}
+                    >
+                        <motion.div
+                            variants={dropIn}
+                            initial='hidden'
+                            animate='active'
+                            exit='exit'
+                            className={`${className}--content`}
+                        >
+                            <div className={`${className}--header`}>
+                                {title && (
+                                    <h2
+                                        className={`${className}--header--title`}
+                                    >
+                                        {title}
+                                    </h2>
+                                )}
+                                <CloseIcon onClick={() => onModalClose()} />
+                            </div>
+                            <div className={`${className}--children`}>
+                                {children}
+                            </div>
+                        </motion.div>
+                    </motion.div>
                 </div>
-            </div>
-        </div>
+            )}
+        </AnimatePresence>
     );
 };
 
